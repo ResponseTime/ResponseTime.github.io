05c59de6e6492f77207e58f1c3ae76338d6b3f4c
Looper <aayush.bhattacharjee2002@gmail.com>
2022-10-09 18:39:09 +0530

    made some major design changes

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const pic = document.querySelectorAll('.item')
 const button = document.getElementById('button')
-pic.forEach(pi=> {
-    pi.addEventListener('click',()=>{
-        if(pi.style.filter ==='brightness(50%)'){
-            pi.style.filter = "brightness(100%)"
-        }
-        else{
-            pi.style.filter = "brightness(50%)"
-        }
-    })
-})
+
 button.addEventListener('click',()=>{
     window.location.replace('index.html')
 })
